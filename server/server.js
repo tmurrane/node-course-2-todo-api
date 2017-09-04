4dba76661a076a5bf3a1ce18c9171b50b0019860
Thomas Murrane <tmurrane@Thomas-Murranes-iMac.local>
2017-09-04 18:36:35 -0500

    Add POST todos and refactor mongo

---
+++ server/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+
+
+
+// var newTodo = new Todo({
+//   text: '    Edit this video    '
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('unable to save');
+// });
+
+// User model - email - require, trim, string, minlength of 1
+// var newUser = new User({
+//   email: ' tom@gmail.com '
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save User',e);
+// });
