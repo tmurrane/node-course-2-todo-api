58357ca3c6ad5fb157589768fc665f3d9bb2499b
Thomas Murrane <tmurrane@Thomas-Murranes-iMac.local>
2017-09-06 21:28:16 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +28,22 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(404).send());
 });
 
 app.listen(3000, () => {
