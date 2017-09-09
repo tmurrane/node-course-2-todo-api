bc5f55357595f62e8a079f98f5b044916a4d85ef
Thomas Murrane <tmurrane@Thomas-Murranes-iMac.local>
2017-09-09 09:43:49 -0500

    Add PATCH /todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -65,6 +66,32 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']); //gets properties that can be updated
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
