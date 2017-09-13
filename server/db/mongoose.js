02c19099a786ac874695cd615db695a06c0a50df
Thomas Murrane <tmurrane@Thomas-Murranes-iMac.local>
2017-09-12 19:15:46 -0500

    Setup separate test and dev

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose: mongoose
 };
+
+// process.env.NODE_ENV === 'production', 'test', 'development'
