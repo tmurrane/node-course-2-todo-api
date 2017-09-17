0663c1d71bf6c76fa214bfddd2dc116e461e9369
Thomas Murrane <tmurrane@Thomas-Murranes-iMac.local>
2017-09-17 11:01:58 -0500

    Create test case for logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -313,4 +313,29 @@ describe('POST /users/login', () => {
   });
 });
 
+describe('DELETE /users/me/token', () => {
+  it('should remove x-auth token on logout', (done) => {
+    //delete request /users/me/token
+    //set x-auth = to token
+    //200
+    // find user, verify tokens arrary length of zero
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
 });
